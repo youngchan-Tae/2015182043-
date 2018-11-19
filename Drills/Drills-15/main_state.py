a4071b07a13e250448a4d1b4a15073d394adb36c
youngchan-Tae(notebook) <1140116@naver.com>
2018-11-19 17:05:29 +0900

    drill15
    캐릭터 벽 충돌체크 추가

--- Drills/Drills-15/main_state.py
+++ Drills/Drills-15/main_state.py
@@ -3,32 +3,35 @@ import json
 import os
 
 from pico2d import *
-
 import game_framework
 import game_world
 
-from background import Background
-from penguin import Penguin
+from boy import Boy
+from background import FixedBackground as Background
+
 
 name = "MainState"
 
-penguin = None
+boy = None
 background = None
-font = None
 
 
 def enter():
-    global penguin, background
-    penguin = Penguin()
+    global boy
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+    global background
     background = Background()
     game_world.add_object(background, 0)
-    game_world.add_object(penguin, 1)
+
+    background.set_center_object(boy)
+    boy.set_background(background)
+
 
 
 def exit():
     game_world.clear()
-
-
 
 def pause():
     pass
@@ -43,20 +46,23 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.quit()
         else:
-            penguin.handle_event(event)
-
+            boy.handle_event(event)
 
 
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
 
+
 def draw():
     clear_canvas()
     for game_object in game_world.all_objects():
         game_object.draw()
     update_canvas()
+
 
 
 
--- project/background.py
+++ project/background.py
@@ -1,12 +1,19 @@
 from pico2d import *
+import  game_framework
+import  game_world
 
 
-class Background0:
+TIME_PER_MOVE = 0.5
+MOVE_PER_TIME = 1.0 / TIME_PER_MOVE
+FRAMES_PER_MOVE = 7
+
+
+class Background:
     def __init__(self):
         self.image = load_image('project_background.png')
         self.frame = 0
     def update(self):
-        self.frame = (self.frame + 1) % 2
+        self.frame = (self.frame + 1 + FRAMES_PER_MOVE  * MOVE_PER_TIME * game_framework.frame_time ) % 2
 
     def draw(self):
         self.image.clip_draw(800 * self.frame, 0, 800, 600, 400, 300)
